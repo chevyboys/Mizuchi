2cd403be03e41047e8a3ba3bb7371fcf54046f0f
Nick Chevalier <volacanicchevy@gmail.com>
2021-11-24 18:29:44 -0700

    dont forget to continue

--- modules/register.js
+++ modules/register.js
@@ -77,7 +77,9 @@ const Module = new Augur.Module()
         for (const file of registryFiles) {
             if (file.indexOf(".js") > -1) {
                 let fileToRegister = file;
-                if(!msg.guild.commands.cache.filter(c => c.name == file.name).size > 0)
+                if(msg.guild.commands.cache.filter(c => c.name == file.name).size > 0) {
+                    continue;
+                }
                 let clientID = Module.client.user.id
                 let guildID = snowflakes.guilds.PrimaryServer
                 const apiEndpoint = `https://discord.com/api/v8/applications/${clientID}/guilds/${guildID}/commands`;
