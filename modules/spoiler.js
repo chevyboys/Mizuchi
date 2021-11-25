d2bb4937eb94a8a5fdcd0fd5e6e89ce26e932bcc
Nick Chevalier <volacanicchevy@gmail.com>
2021-11-24 19:13:15 -0700

    smol bug fix

--- modules/spoiler.js
+++ modules/spoiler.js
@@ -21,7 +21,7 @@ async function spoilerMsg(msg) {
            }
         }
     }
-    if(msg.attachements.size > -1){
+    if(msg.attachements.length > 0){
         newMessage.attachements = msg.attachements;
         for (const attachement of newMessage.attachements ) {
             if(attachement) {
