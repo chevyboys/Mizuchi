dfd86b72048785db153dcaf76bd0042f621aa20e
chevyboys <volcanicchevy@gmail.com>
2021-11-28 14:38:30 -0700

    don't forget to be async

--- modules/register.js
+++ modules/register.js
@@ -31,7 +31,7 @@ let restrict = async (command, allowedRoles) => {
 }
 
 const Module = new Augur.Module()
-.setInit(() =>  {
+    .setInit(async () => {
         // Build slash commands
         commands = [
             new SlashCommandBuilder()
