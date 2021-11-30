9d4a6ea1f627b993bd9e952b428510e6ec3b93ee
Nick Chevalier <volacanicchevy@gmail.com>
2021-11-30 00:43:44 -0700

    maybe don't throw errors now?

--- registry/staff.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-command =
-    new SlashCommandBuilder()
-        .setName('staff')
-        .setDescription('get a list of the current server staff members')
-        .toJSON();
-
-module.exports = commands;
