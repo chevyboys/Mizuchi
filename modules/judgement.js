5f97869737424d19d266d27475a02b7fbc1e92a5
Nick Chevalier <volacanicchevy@gmail.com>
2021-12-27 17:58:24 -0700

    Get ready for room one development

--- modules/judgement.js
+++ modules/judgement.js
@@ -5,17 +5,27 @@ const Augur = require("augurbot"),
     Attunements = require('../Judgement/AttunementRoles.json'),
     AUtils = require("../Judgement/Attunement Roles Utils")
 
-let selectSpire = async (interaction) => {
-    interaction.reply({content: `you have selected the ${interaction.value} spire`, ephemeral: true})
+let activeAttunements = [];
+
+//general helper functions
+let buildGenericEmbed = (interaction) => {
+    return u.embed()
+        .setAuthor({
+            name: interaction.member?.displayName || interaction.author?.username, 
+            iconURL: interaction.author?.displayAvatarURL({ size: 16 }),
+            url: interaction.url
+        })
+        .setColor(interaction.member?.displayColor)
+
 }
 
-
+//Handlers for selecting your spire
+let buildSelectSpire = async (interaction, replyInsteadOfUpdate) => {
+    if (!replyInsteadOfUpdate) interaction.update({ embeds: [buildSpireSelectEmbed(interaction)], components: buildSpireSelectComponents(interaction, false), ephemeral: true })
+    else await interaction.reply({ embeds: [buildSpireSelectEmbed(interaction)], components: buildSpireSelectComponents(interaction, false), ephemeral: true })
+}
 let buildSpireSelectEmbed = (interaction) => {
-    return u.embed()
-        .setAuthor(interaction.member?.displayName || interaction.author?.username, interaction.author?.displayAvatarURL({ size: 16 }), interaction.url)
-        .setDescription("Your judgement begins. Please select a spire.")
-        .setColor(interaction.member?.displayColor)
-        .setTimestamp(interaction.createdAt);
+    return buildGenericEmbed(interaction).setDescription("Your judgement begins. Please let us know which spire you will be entering. Be warned, this choice once made cannot be undone.")
 }
 let buildSpireSelectComponents = (interaction) => {
     let SelectMenuOptions = [];
@@ -27,6 +37,11 @@ let buildSpireSelectComponents = (interaction) => {
             value: spire,
         })
     }
+    SelectMenuOptions.push({
+        label: "Random",
+        description: `A Random spire`,
+        value: "Random",
+    })
     let rows = []
     const row = new MessageActionRow()
         .addComponents(
@@ -38,6 +53,58 @@ let buildSpireSelectComponents = (interaction) => {
     rows.push(row);
     return rows;
 }
+//handlers for when the spire has been selected
+let buildSpireHasBeenSelectedEmbed = (interaction, spire) => {
+    return buildGenericEmbed(interaction).setDescription(`You travel to the ${spire} spire.`)
+}
+let buildSpireHasBeenSelectedComponents = (interaction, spire) => {
+    row = new MessageActionRow()
+    row.addComponents(
+        new MessageButton()
+            .setCustomId(`spireHasBeenSelectedEnter`)
+            .setLabel(`Enter the ${spire} spire`)
+            .setStyle('SUCCESS')
+    )
+    row.addComponents(
+        new MessageButton()
+            .setCustomId(`spireHasBeenSelectedRetreat`)
+            .setLabel(`Nope, Start over`)
+            .setStyle('DANGER')
+    )
+    return [row];
+}
+
+let buildSpireHasBeenSelected = async (interaction) => {
+    let spire;
+    if(interaction.values[0].toLowerCase().indexOf("random") > -1) spire = AUtils.spires()[Math.floor(Math.random() * AUtils.spires().length)]
+    else spire = interaction.values[0];
+    interaction.update({embeds: [buildSpireHasBeenSelectedEmbed(interaction, spire)], components:buildSpireHasBeenSelectedComponents(interaction, spire), ephemeral: true})
+}
+
+//handle room one
+
+let roomOneEmbed = (interaction) => {
+    return buildGenericEmbed(interaction).setDescription(`The spire is currently closed`)
+}
+let roomOneComponents = (interaction) => {
+    row = new MessageActionRow()
+    row.addComponents(
+        new MessageButton()
+            .setCustomId(`spireHasBeenSelectedEnter`)
+            .setLabel(`Try Again`)
+            .setStyle('PRIMARY')
+    )
+    row.addComponents(
+        new MessageButton()
+            .setCustomId(`spireHasBeenSelectedRetreat`)
+            .setLabel(`Start over`)
+            .setStyle('DANGER')
+    )
+    return [row];
+}
+let roomOne = async (interaction) => {
+    interaction.update({embeds: [roomOneEmbed(interaction)], components: roomOneComponents(interaction), ephemeral: true})
+}
 
 const Module = new Augur.Module()
     .addInteractionCommand({
@@ -45,10 +112,12 @@ const Module = new Augur.Module()
         guildId: snowflakes.guilds.PrimaryServer,
         process: async (interaction) => {
             // Akn
-            await interaction.reply({ embeds: [buildSpireSelectEmbed(interaction)], components: buildSpireSelectComponents(interaction, false), ephemeral: true });
+            buildSelectSpire(interaction, true);
         }
     })
-    .addInteractionHandler({ customId: `JudgementSpireSelect`, process: selectSpire });
+    .addInteractionHandler({ customId: `JudgementSpireSelect`, process: buildSpireHasBeenSelected })
+    .addInteractionHandler({ customId: `spireHasBeenSelectedEnter`, process: roomOne })
+    .addInteractionHandler({ customId: `spireHasBeenSelectedRetreat`, process: buildSelectSpire });
 
 
 
