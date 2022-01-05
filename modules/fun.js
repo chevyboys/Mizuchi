e0538d8e4c9bbf1ded1f6cbb98e50a5307662c17
Nick Chevalier <volacanicchevy@gmail.com>
2022-01-04 21:48:00 -0700

    cleaing up fun.js to match style guide

--- modules/fun.js
+++ modules/fun.js
@@ -3,15 +3,11 @@ const Augur = require("augurbot"),
     snowflakes = require('../config/snowflakes.json');
 const mee6 = "848863263218728980"
 const Module = new Augur.Module()
-.addEvent("messageCreate", async (msg) => {
-    if(msg.author.id != mee6 || !(msg.content.indexOf("2.5k birthday xp.") > -1) || msg.channel.id != snowflakes.channels.general) return;
-    else {
-        msg.channel.send("Radiance wishes you a happy day of cake. 🎂")
-    }
-
-});
-
-
-
-
+//Reply to mee6 cake day messages
+    .addEvent("messageCreate", async (msg) => {
+        if (msg.author.id != mee6 || !(msg.content.indexOf("2.5k birthday xp.") > -1) || msg.channel.id != snowflakes.channels.general) return;
+        else {
+            msg.channel.send("Radiance wishes you a happy day of cake. 🎂")
+        }
+    });
 module.exports = Module;
