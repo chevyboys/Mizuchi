302201c014c46338e2a0a0e22a09c3a2c95f59db
chevyboys <volcanicchevy@gmail.com>
2022-01-28 15:35:08 -0700

    additional type checking on DB update

--- utils/Utils.Database.js
+++ utils/Utils.Database.js
@@ -128,7 +128,7 @@ let privateDataBaseActions = {
             //only set prooperties that we already have in the database as a colum. If not specified, leave it the same.
             let query = `UPDATE users SET `
             for (const property in user) {
-                if (property != "userID" && typeof user[property] != "function") {
+                if (property != "userID" && typeof user[property] != "function" && !(user[property] instanceof Function) && Object.prototype.toString.call(user[property]) != '[object Function]') {
                     user[property] = userDataBaseObject[property] || user[property];
                     if (typeof user[property] === 'string' || user[property] instanceof String) {
                         query = query + ` ${property} = ${con.escape(user[property])},`
