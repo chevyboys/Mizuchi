dcaa25a979fe15e4d53168af32964fddb349c1a0
chevyboys <volcanicchevy@gmail.com>
2022-02-08 15:56:25 -0700

    More memes

--- modules/Module.CommandInteraction.ServerInfo.js
+++ modules/Module.CommandInteraction.ServerInfo.js
@@ -3,6 +3,29 @@ const Augur = require("augurbot"),
 const { BaseMessageComponent, MessageButton, MessageActionRow } = require("discord.js");
 const snowflakes = require('../config/snowflakes.json');
 
+
+let memeLinks = [
+    "https://cdn.discordapp.com/attachments/697869453605601352/933133090249908234/Iceberg_26122021104304.jpg",
+    "https://cdn.discordapp.com/attachments/697869453605601352/931422425919389756/received_269847078157064.jpeg",
+    "https://cdn.discordapp.com/attachments/697869453605601352/924898168607494165/SPOILER_aged_like_fine_wine.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/924771025160384572/Polish_20211226_1406251742.jpg",
+    "https://media.discordapp.net/attachments/824862207270977557/920717254377762866/ezgif.com-gif-maker_24.gif",
+    "https://cdn.discordapp.com/attachments/697869453605601352/911964611476094986/unknown.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/911473470045564928/unknown.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/909373921323782164/unknown.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/908126909366419456/SPOILER_unknown.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/908094210622783569/SPOILER_unknown.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/907482456813350952/SOUBREAD2.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/903140346840633394/5s3ft3.jpg",
+    "https://cdn.discordapp.com/attachments/697869453605601352/901271050338320394/ZomboMeme_23102021061857.jpg",
+    "https://cdn.discordapp.com/attachments/697869453605601352/898826116255608842/Jin_nosee.png",
+    "https://cdn.discordapp.com/attachments/697869453605601352/895890645699407962/zgni7pn5l9u512.png",
+    "https://www.youtube.com/watch?v=bxqLsrlakK8",
+    "https://www.youtube.com/watch?v=k4h6WK0D4O8",
+    "https://www.youtube.com/watch?v=kCXzb1BGRrs"
+]
+
+
 const Module = new Augur.Module()
     .addInteractionCommand({
         name: "staff",
@@ -102,7 +125,7 @@ const Module = new Augur.Module()
                         disabled: false,
                         label: "Adorable Cockatrices",
                         style: "LINK",
-                        url: "https://www.youtube.com/watch?v=bxqLsrlakK8"
+                        url: memeLinks[Math.floor(Math.random() * memeLinks.length)]
                     }));
 
             await interaction.reply({ content: "**__Cool things to check out:__**", components: [components], ephemeral: false });
