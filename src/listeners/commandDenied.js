22d42d7db6cafc4e477e80bbe380b02d5020d780
chevyboys <volcanicchevy@gmail.com>
2022-04-08 18:17:03 -0600

    Fix CommandDeniedListener

--- src/listeners/commandDenied.js
+++ src/listeners/commandDenied.js
@@ -2,8 +2,7 @@ const { Listener } = require('@sapphire/framework');
 
 class CommandDeniedListener extends Listener {
   run(error, { message }) {
-    u.log(error)
-    if (error.context.silent) return;
+    if (error.context?.silent) return;
     return message.channel.send(error.message);
   }
 }
