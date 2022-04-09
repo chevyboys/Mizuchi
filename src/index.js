3c7d4bcee69f2abf7df6234748b3ff3c02f012a7
chevyboys <volcanicchevy@gmail.com>
2022-04-09 05:43:06 -0600

    add logging to discord webhook (via intercept) Fixes #92

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 require('@sapphire/plugin-editable-commands/register');
+require('@sapphire/plugin-logger/register');
 const { SapphireClient } = require('@sapphire/framework');
 const config = require("../config/config.json")
 const U = require("./utilities/General")
+const snowflakes = require("../config/snowflakes.json")
 
+//handle intercepting logs:
+const originalStdoutWrite = process.stdout.write.bind(process.stdout);
+process.stdout.write = (chunk, encoding, callback) => {
+    if (typeof chunk === 'string') {
+        U.log(chunk)
+    }
+    return originalStdoutWrite(chunk, encoding, callback);
+};
+//handle intercepting errors
+const originalStdErr = process.stderr.write.bind(process.stderr);
+process.stderr.write = (chunk, encoding, callback) => {
+    if (typeof chunk === 'string') {
+        U.errorHandler({error: chunk, fromConsole:true})
+    }
+
+    return originalStdErr(chunk, encoding, callback);
+}
+
+//register the client
 const client = new SapphireClient({
-     intents: ['GUILDS', 'GUILD_MESSAGES'],
-     defaultPrefix: config.prefix,
-    });
+    intents: ['GUILDS', 'GUILD_MESSAGES'],
+    defaultPrefix: config.prefix,
+    disableMentionPrefix: true,
+    loadDefaultErrorListeners: true,
+    caseInsensitiveCommands: true,
+    caseInsensitivePrefixes: true,
+});
+let start = async () => {
+    await client.login(config.Tokens.primary);
+    await client.guilds.fetch(snowflakes.guilds.PrimaryServer);
+    U.client = client;
+    console.log("Login Successful");
+}
+start()
 
-client.login(config.Tokens.primary);
-U.client = client;
-console.log("Login Successful");
+process.on("unhandledRejection", (error, p) => p.catch(e => U.errorHandler(e, "Unhandled Rejection")));
+process.on("uncaughtException", (error) => U.errorHandler(error, "Uncaught Exception"));
