4b355626cbc85784310e119706aff44a6dc4fd5d
norablackcat <blackcatgames@protonmail.com>
2022-11-08 00:49:23 -0700

    Queue and priority queue working

--- tests/Utils.js
+++ tests/Utils.js
@@ -1,16 +1,36 @@
 const test = require('ava');
-const { Queue } = require('../utils/Utils.Queue.js');
+const { Queue, PriorityQueue } = require('../utils/Utils.Data.js');
+const cats = require('./data/cats.json').cats;
 
 test('Queue', t => {
-  const cats = new Queue();
-  cats.enqueue('Jin');
+  const queue = new Queue();
+  queue.push(cats[0]);
 
-  t.is('Jin', cats.peek());
+  t.is('Jin', queue.peek());
 
 });
 
-// test('Priority Queue', t => {
+test('Priority Queue', t => {
+  const numbers = [];
+  for (let i = 0; i < 100; i++) {
+    numbers.push(Math.random());
+  }
+  const pqueue = new PriorityQueue();
+  numbers.forEach(number => {
+    pqueue.push(number);
+  })
+  let result = [];
 
-// });
+  while (!pqueue.isEmpty()) {
+    result.push(pqueue.pop());
+  }
 
-// test('')
+  console.log([...numbers].sort((a, b) => b - a));
+
+
+  pqueue.pop();
+  console.log(pqueue.items);
+
+  t.is([...numbers].sort((a, b) => b - a), result);
+
+})
