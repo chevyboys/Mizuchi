15ff0ec5b7be46710cd65659ffbdbff43a17eb5f
chevyboys <volcanicchevy@gmail.com>
2022-11-10 01:29:14 -0700

    pushing to test on secondary server

--- modules/Module.CommandInteraction.Roles.js
+++ modules/Module.CommandInteraction.Roles.js
@@ -10,8 +10,8 @@ Module.addInteractionCommand({
     interaction.deferReply();
     let memberColors = await roleUtilities.getMemberColorInventory(interaction.member);
     let memberSecondary = await roleUtilities.getSecondaryInventory(interaction.member);
-    let memberColorString = memberColors.map(c => `<@&${c}>` + interaction.member.roles.cache.has(c) ? " ✅" : "").join("\n");
-    let memberSecondaryString = memberSecondary.map(c => `<@&${c}>` + interaction.member.roles.cache.has(c) ? " ✅" : "").join("\n");
+    let memberColorString = memberColors.map(c => `<@&${c}>` + (interaction.member.roles.cache.has(c) ? " ✅" : "")).join("\n");
+    let memberSecondaryString = memberSecondary.map(c => `<@&${c}>` + (interaction.member.roles.cache.has(c) ? " ✅" : "")).join("\n");
     let embed = u.embed()
       .setTitle(interaction.member.displayName + "'s inventory")
       .setDescription(`__**Equipable Colors:**__\n${memberColorString}\n\n__**Other Equipable roles**__\n${memberSecondaryString}`);
--- utils/Utils.RoleInventory.js
+++ utils/Utils.RoleInventory.js
@@ -12,17 +12,21 @@ const roleUtilities = {
     if (role.roleToInheritFrom != '') {
       roleColorsProvided.push(...await roleUtilities.getColorsProvidedByRole(role.roleToInheritFrom))
     }
+
     if (role.colorInventory) {
-      if (typeof role.colorInventory === 'string') roleColorsProvided.push(role.colorInventory);
+      if (typeof role.colorInventory === 'string') {
+        let colorInventory = role.colorInventory.split("\n").map(c => c.replace("\n", ""));
+        roleColorsProvided.push(colorInventory)
+      }
       else roleColorsProvided.push(...role.colorInventory)
     }
     return roleColorsProvided;
   },
   getMemberColorInventory: async (member) => {
-    const roles = await getGoogleSheetAsJSON(snowflakes.sheets.roles)
+    const roles = await getGoogleSheetAsJSON(snowflakes.sheets.roles);
     let memberRoles = member.roles.cache.filter(r => roles.filter(sheetRole => sheetRole.colorInventory).map(sr => sr.id).includes(r.id));
     let roleInventory = (await (await Promise.all(memberRoles.map((r) => roleUtilities.getColorsProvidedByRole(r.id, roles))))).flat();
-    return roleInventory;
+    return roleInventory.flat(1);
   },
   getSecondaryRolesProvidedByRole: async (roleid, sheetRoleArray) => {
     const roles = sheetRoleArray || await getGoogleSheetAsJSON(snowflakes.sheets.roles);
@@ -33,7 +37,10 @@ const roleUtilities = {
       rolesProvided.push(... await roleUtilities.getColorsProvidedByRole(role.roleToInheritFrom))
     }
     if (role.generalInventory) {
-      if (typeof role.generalInventory === 'string') rolesProvided.push(role.generalInventory);
+      if (typeof role.generalInventory === 'string') {
+        let generalInventory = role.generalInventory.split("\n").map(c => c.replace("\n", ""));
+        rolesProvided.push(generalInventory);
+      }
       else rolesProvided.push(...role.generalInventory)
     }
     return rolesProvided;
@@ -42,7 +49,7 @@ const roleUtilities = {
     const roles = await getGoogleSheetAsJSON(snowflakes.sheets.roles)
     let memberRoles = member.roles.cache.filter(r => roles.filter(sheetRole => sheetRole.generalInventory).map(sr => sr.id).includes(r.id));
     let roleInventory = (await (await Promise.all(memberRoles.map((r) => roleUtilities.getColorsProvidedByRole(r.id, roles))))).flat();
-    return roleInventory;
+    return roleInventory.flat(1);
   }
 
 }
