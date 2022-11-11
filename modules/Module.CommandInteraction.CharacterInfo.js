ba0586c046f90826f6c5c8c59d985bad51ee9948
norablackcat <blackcatgames@protonmail.com>
2022-11-11 02:14:55 -0700

    buttons with extra info

--- modules/Module.CommandInteraction.CharacterInfo.js
+++ modules/Module.CommandInteraction.CharacterInfo.js
@@ -1,7 +1,7 @@
 const Augur = require("augurbot"),
   gs = require("../utils/Utils.GetGoogleSheetsAsJson.js"),
   u = require("../utils/Utils.Generic.js"),
-  fs = require("fs");
+  { MessageActionRow, MessageButton, ButtonStyle } = require("discord.js");
 const snowflakes = require('../config/snowflakes.json');
 
 let characterInfo;
@@ -24,28 +24,81 @@ async function findCharacter(character) {
 
 async function buildMessage(index) {
 
-  let out = "";
+  let msg = "";
+  let buttonRow = new MessageActionRow();
+  let buttonEmpty = true;
   if (index != undefined) {
     const spoiler = new RegExp('\|\|');
 
     for (const [key, value] of Object.entries(characterInfo[index])) {
-      if (value && key != "Character Name" && !key.startsWith("_")) {
+      if (value && key != "Character Name" && !key.startsWith("_") && !key.startsWith("*")) {
         const backticks = !spoiler.test(value) ? "```" : " ";
-        out += "__**" + key + "**__\n" + backticks + value + " " + backticks + "\n";
+        msg += "__**" + key + "**__\n" + backticks + value + " " + backticks + "\n";
+      }
+
+      // ideally this should be in a different function buildButton but a bit of refactoring is required to not have two loops.
+      if (value && key.startsWith("*")) {
+        buttonEmpty = false;
+        if (key != "*wikiUrl") {
+          buttonRow.addComponents(
+            new MessageButton()
+              .setCustomId(key)
+              .setLabel("Show " + key.split("*"))
+              .setStyle("PRIMARY")
+          );
+        } else {
+          buttonRow.addComponents(
+            new MessageButton()
+              .setLabel("Wiki")
+              .setStyle("LINK")
+              .setURL(value)
+          );
+        }
+
       }
     }
+    if (buttonEmpty) {
+      buttonRow.addComponents(
+        new MessageButton()
+          .setLabel("Edit Sheet")
+          .setStyle("LINK")
+          .setURL("https://docs.google.com/spreadsheets/d/1mOYLi95O9es2NDq0JTJ9xL5FVzDbwUvoahMrtdp0icg/")
+      );
+    }
   } else {
-    return u.embed().setTitle("Could not find character");
+    buttonRow.addComponents(
+      new MessageButton()
+        .setLabel("Edit Sheet")
+        .setStyle("LINK")
+        .setURL("https://docs.google.com/spreadsheets/d/1mOYLi95O9es2NDq0JTJ9xL5FVzDbwUvoahMrtdp0icg/")
+    );
+    return { embeds: [u.embed().setTitle("Could not find character, feel free to add them yourself! \n If this message is a mistake contact Nora")], components: [buttonRow] };
   }
 
   const embed = u.embed().setAuthor({
     name: characterInfo[index]["Character Name"],
     url: characterInfo[index]["_wikiUrl"] || undefined,
     iconURL: characterInfo[index]["_imageUrl"] || undefined,
-  }).setDescription(out);
+  }).setDescription(msg);
   if (characterInfo[index]["_Color Resolvable"])
     embed.setColor(characterInfo[index]["_Color Resolvable"]);
-  return embed;
+
+  return { embeds: [embed], components: [buttonRow] };
+}
+
+async function extraInfo(interaction) {
+  const char = characterInfo.find(obj => {
+    return obj["Character Name"] == interaction.message.embeds[0].author.name;
+  })
+  console.log(char[interaction.customId])
+
+
+
+  // interaction.components.forEach(element => {
+  //   element.set
+  // });
+  // interaction.deferReply(interaction);
+  interaction.reply({ content: char[interaction.customId] });
 }
 
 const Module = new Augur.Module();
@@ -61,9 +114,14 @@ Module.setInit(async () => {
 
     const embed = await buildMessage(await findCharacter(character));
     // console.log(JSON.stringify(characterInfo));
-    interaction.editReply({ embeds: [embed] });
+    interaction.editReply(embed);
   }
 
+}).addInteractionHandler({
+  customId: `*Description`, process: extraInfo
+}).addInteractionHandler({
+  customId: `*Trivia`, process: extraInfo
 })
+
 
 module.exports = Module;
