e2847493cb162628b59ec625fd9e29850b5493d5
chevyboys <volcanicchevy@gmail.com>
2022-11-17 00:32:13 -0700

    fixes

--- modules/Module.CommandInteraction.Roles.js
+++ modules/Module.CommandInteraction.Roles.js
@@ -56,14 +56,14 @@ Module.addInteractionCommand({
 })
   .addInteractionHandler({
     customId: `InventoryRoleSelect`, process: async (interaction) => {
-      interaction.deferUpdate();
+      interaction.deferReply();
       let memberColors = await roleUtilities.getMemberColorInventory(interaction.member);
       let color;
       if (interaction.values[0].toLowerCase().indexOf("random") > -1) color = memberColors[Math.floor(Math.random() * memberColors.length)]
       else color = interaction.values[0];
       await u.addRoles(interaction.member, memberColors, true);
       u.addRoles(interaction.member, color);
-      interaction.update({ content: "You have successfully selected a role", components: roleMessageComponents(interaction, memberColors), ephemeral: true })
+      interaction.reply({ content: "You have successfully selected a role", ephemeral: true })
     }
   })
 
