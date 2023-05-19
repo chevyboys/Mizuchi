2e79732863a361794796d8c1d6fd9e04c32ad2e8
chevyboys <volcanicchevy@gmail.com>
2023-05-18 21:56:45 -0600

    Fleshing out the database schema for guilds

---
+++ src/utils/Database.ts/prismaClient.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+prisma.$connect();
+
+export { prisma as prismaClient };
+
+
+process.on("beforeExit", async () => { prisma.$disconnect(); } ); 
