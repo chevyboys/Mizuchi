048c0d775a691b6681e84c15ec481b6e6d640b0e
chevyboys <volcanicchevy@gmail.com>
2023-05-26 21:38:42 -0600

    Small formatting fix

--- src/modules/database/createGuild.ts
+++ src/modules/database/createGuild.ts
@@ -15,39 +15,39 @@ let guildChannelsBuilder = new SlashCommandBuilder()
         subcommand
             .setName('channels')
             .setDescription('sets the channels for the guild')
-            for (const key in emptyChannels) {
-                if(typeof emptyChannels[key] != "string" && typeof emptyChannels[key] != "undefined" && typeof emptyChannels[key] != null && typeof emptyChannels[key] != "object") continue;
-                subcommand.addChannelOption(option => option.setName(keyToDiscordChannelOptionName(key)).setDescription("The channel you would like to set as the " + keyToDiscordChannelOptionName(key)));
-            }
-            return subcommand;
+        for (const key in emptyChannels) {
+            if (typeof emptyChannels[key] != "string" && typeof emptyChannels[key] != "undefined" && typeof emptyChannels[key] != null && typeof emptyChannels[key] != "object") continue;
+            subcommand.addChannelOption(option => option.setName(keyToDiscordChannelOptionName(key)).setDescription("The channel you would like to set as the " + keyToDiscordChannelOptionName(key)));
+        }
+        return subcommand;
     }
     )
 
 
 let subcommandChannelsProccess = async (interaction: ChatInputCommandInteraction) => {
-    if(!interaction.guildId) return console.error("no guild id");
-    if(!interaction.guild) return console.error("no guild");
+    if (!interaction.guildId) return console.error("no guild id");
+    if (!interaction.guild) return console.error("no guild");
     let selectedChannels: DbGuildChannels = new DbGuildChannels();
-        for (const key in emptyChannels) { 
-            interaction.options.getChannel(keyToDiscordChannelOptionName(key)) ? selectedChannels[key as keyof DbGuildChannels] = interaction.options.getChannel(keyToDiscordChannelOptionName(key))?.id : null;
-        }
-        let raw = await DbGuild.create(interaction.guild)
-        let guild = new DbGuild(raw);
-        console.log(await guild.setChannels(selectedChannels));
-        let newGuild = await DbGuild.get(interaction.guildId);
-        guild = new DbGuild(newGuild? newGuild : guild);
+    for (const key in emptyChannels) {
+        interaction.options.getChannel(keyToDiscordChannelOptionName(key)) ? selectedChannels[key as keyof DbGuildChannels] = interaction.options.getChannel(keyToDiscordChannelOptionName(key))?.id : null;
+    }
+    let raw = await DbGuild.create(interaction.guild)
+    let guild = new DbGuild(raw);
+    console.log(await guild.setChannels(selectedChannels));
+    let newGuild = await DbGuild.get(interaction.guildId);
+    guild = new DbGuild(newGuild ? newGuild : guild);
 
-        let embed = new EmbedBuilder()
+    let embed = new EmbedBuilder()
         .setTitle("Channel Setup")
         .setDescription("The channels have been set to the following:\n" + guild.channels.toString())
         .setColor([255, 3, 5])
         .setTimestamp();
 
-        interaction.isRepliable() ? interaction.reply({
-            embeds: [
-                embed
-            ]
-        }) : console.error("could not reply");
+    interaction.isRepliable() ? interaction.reply({
+        embeds: [
+            embed
+        ]
+    }) : console.error("could not reply");
 }
 
 
@@ -55,15 +55,16 @@ export let SetupSlashCommand = new SlashCommandComponent({
     builder: guildChannelsBuilder,
     enabled: true,
     category: "setup",
-    permissions: (interaction) => { 
+    permissions: (interaction) => {
         return interaction.memberPermissions?.has("Administrator")
-     || interaction.memberPermissions?.has("ManageChannels") 
-     || interaction.memberPermissions?.has("ManageGuild") || false },
+            || interaction.memberPermissions?.has("ManageChannels")
+            || interaction.memberPermissions?.has("ManageGuild") || false
+    },
     process: async (interaction) => {
 
-        if(interaction.options.getSubcommand() == "channels") {
+        if (interaction.options.getSubcommand() == "channels") {
             subcommandChannelsProccess(interaction);
         }
-        
+
     }
 })
