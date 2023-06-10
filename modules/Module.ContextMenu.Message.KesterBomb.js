4c1626874553edbd72f75815b6445ee44f65cce2
chevyboys <volcanicchevy@gmail.com>
2023-06-10 09:44:09 -0600

    Handle Kesterbomb overflow

--- modules/Module.ContextMenu.Message.KesterBomb.js
+++ modules/Module.ContextMenu.Message.KesterBomb.js
@@ -1,56 +1,68 @@
 const Augur = require("augurbot"),
   modRequest = require('../utils/Utils.ContextMenu.Message.ModCard'),
-  config = require("../config/config.json"),
-  snowflakes = require("../config/snowflakes.json");
+  u = require("../utils/Utils.Generic");
+snowflakes = require("../config/snowflakes.json");
 const { MessageButton, MessageActionRow, WebhookClient } = require('discord.js');
 const webhookUtil = require("../utils/Webhook").webhook;
 const Module = new Augur.Module()
-//const kesterBombHook = new WebhookClient(config.Webhooks.kesterBombs);
 
 
-async function buildMessage(messageToBeBombed, color) {
-  const url = messageToBeBombed.url;
+
+async function buildMessage(messageToBeBombed) {
   const guild = Module.client.guilds.cache.get(snowflakes.guilds.PrimaryServer)
   //recurse through all responses in a chain
   let returnObj = [];
   if (messageToBeBombed.reference) {
     let referencedMessageChannel = await guild.channels.fetch(messageToBeBombed.reference.channelId);
     let referencedMessage = await referencedMessageChannel.messages.fetch(messageToBeBombed.reference.messageId);
-    returnObj = await buildMessage(referencedMessage, color);
+    returnObj = await buildMessage(referencedMessage);
   }
   let files = messageToBeBombed.attachments ? Array.from(messageToBeBombed.attachments.values()).map(v => v.attachment) : null
-  const components = [new MessageActionRow()
-    .addComponents(
-      new MessageButton({
-        disabled: false,
-        label: "Original Message",
-        style: "LINK",
-        url: url
-      }))
-  ]
-  //if (messageToBeBombed.content) messageToBeBombed.embeds?.unshift(embed);
+
+
   returnObj.push({
-    content: (messageToBeBombed.content ? ">>> " + messageToBeBombed.content : "") + "\n\n" + url,
+    content: (messageToBeBombed.content ? messageToBeBombed.content : ""),
     username: (await messageToBeBombed.member)?.displayName || messageToBeBombed.author.username,
     avatarURL: (await messageToBeBombed.member)?.avatarURL() || (await messageToBeBombed.author)?.avatarURL(),
     embeds: messageToBeBombed.embeds,
     allowedMentions: { parse: [] },
     files: files,
-    components: components
+    url: messageToBeBombed.url
   })
   return returnObj;
 }
 
 async function kesterBomb(inputObject) {
-  let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   const guild = Module.client.guilds.cache.get(snowflakes.guilds.PrimaryServer)
   let channelToBeBombed = guild.channels.cache.get(inputObject.target.channelId);
   let messageToBeBombed = await channelToBeBombed.messages.fetch(inputObject.target.id);
-  let messagesToSend = await buildMessage(messageToBeBombed, color);
+  let messagesToSend = await buildMessage(messageToBeBombed);
   for (let index = 0; index < messagesToSend.length; index++) {
     const messageToSend = messagesToSend[index];
-    //await kesterBombHook.send(messageToSend);
-    await webhookUtil(guild.channels.cache.get(snowflakes.channels.kesterBomb), "kesterBombhook", "./avatar/base.png", messageToSend)
+
+
+    let chunks = u.splitMessage(messageToSend.content)
+    for (let index = 0; index < chunks.length; index++) {
+      const chunk = chunks[index];
+
+      messageToSend.content = ">>> " + chunk;
+      if (index == chunks.length - 1) {
+        messageToSend.content = messageToSend.content + "\n\n" + messageToSend.url;
+      }
+
+
+      try {
+        await webhookUtil(guild.channels.cache.get(snowflakes.channels.kesterBomb), "kesterBombhook", "./avatar/base.png", messageToSend);
+      } catch (error) {
+        if (error.toString().indexOf("Missing Permission") > -1) {
+          await u.errorLog.send({ embeds: [u.embed().setColor("PURPLE").setTitle("Missing Permission").setDescription("I don't have permission to send messages in <#" + snowflakes.channels.kesterBomb + ">. Please give me permission to send messages in that channel.")] });
+        }
+        else {
+          throw error;
+        }
+      }
+
+    }
   }
 
 
