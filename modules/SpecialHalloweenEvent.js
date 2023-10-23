5ffd6828febf0cecea25aa75f7406f93bf6f99c7
chevyboys <volcanicchevy@gmail.com>
2023-10-23 03:42:30 +0000

    quick foix for haunting

--- modules/SpecialHalloweenEvent.js
+++ modules/SpecialHalloweenEvent.js
@@ -115,7 +115,7 @@ Module.addEvent("messageReactionAdd", async (reaction, user) => {
     Math.floor(Math.random() * 100) > 90
   ) {
     msg.react(holidays[0].emoji)
-  } else if (hauntedChannel) {
+  } else if (msg.channel.id == hauntedChannel) {
     msg.react(holidays[0].emoji)
   }
   else {
