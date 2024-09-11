017d1598433d3c0712e4c74658ea017851a2ba5d
chevyboys <volcanicchevy@gmail.com>
2024-09-10 20:50:47 -0600

    small tweak

--- modules/Module.CommandInteraction.Silly.js
+++ modules/Module.CommandInteraction.Silly.js
@@ -706,10 +706,16 @@ const Module = new Augur.Module()
         ];
       }
 
+      if (numberOfTimesPressed == 150) {
+        responses = ["You've pressed the button 150 times. You win. Happy 150th anniversary of the push button."];
+        //get the top 30 people who have pressed the button
+        let top30 = peopleWhoHavePressedTheButton.sort((a, b) => b - a).slice(0, 30);
+        interaction.guild.channels.cache.get(snowflakes.channels.botSpam).send(`<@${interaction.user.id}> won the game. Current leaderboard: \n\n${top30.map((value, key) => `<@${key}>: ${value}`).join("\n")}`);
+      }
       let response = responses[Math.floor(Math.random() * responses.length)];
       interaction.guild.channels.cache.get(snowflakes.channels.secret).send(`<@${interaction.user.id}> pressed the button ${numberOfTimesPressed} times \n\nresponse:\n\`\`\`${response}\`\`\``);
       interaction.reply({
-        content: response,
+        content: response + "\n\n\n You've pressed the button " + numberOfTimesPressed + " times",
         ephemeral: true
       })
     }
