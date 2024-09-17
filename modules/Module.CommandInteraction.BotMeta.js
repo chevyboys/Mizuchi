91d509db6adad7ed0c29da90fd3794f0f014a6c6
chevyboys <volcanicchevy@gmail.com>
2024-09-17 00:12:45 -0600

    fix logging of say

--- modules/Module.CommandInteraction.BotMeta.js
+++ modules/Module.CommandInteraction.BotMeta.js
@@ -294,7 +294,7 @@ Module
       if (msg.deletable && (msg.client.config.AdminIds.includes(msg.author.id) || msg.client.config.ownerId == msg.author.id)) msg.delete();
       let files = msg.attachments ? Array.from(msg.attachments.values()).map(v => v.attachment) : null
       msg.channel.send({ content: suffix, embeds: msg.embeds, files: files, reply: { messageReference: msg.reference?.messageId || null } });
-      u.errorLog(msg.author.username + " in channel " + msg.channel?.name + " said: " + suffix);
+      u.errorHandler(msg.author.username + " in channel " + msg.channel?.name + " said: " + suffix);
     },
     permissions: (msg) => msg.member?.roles.cache.has(snowflakes.roles.BotMaster) || msg.member?.roles.cache.has(snowflakes.roles.BotAssistant) || msg.client.config.AdminIds.includes(msg.author.id) || msg.client.config.ownerId == msg.author.id
   })
