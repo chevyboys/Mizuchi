08794ebfd0d6d86ed9b61c1d40162fa5c79b5206
chevyboys <volcanicchevy@gmail.com>
2024-10-06 01:49:11 -0600

    Add first gift stub

---
+++ modules/Halloween2024/Gift.js
@@ -0,0 +1,25 @@
+/**
+ * Gift Module
+ * Author: jhat0353
+ * Date: 2024
+ */
+
+let gift = {
+  /**
+   * 
+   * @param {Interaction} interaction 
+   * @param {ParticipantManager} participants 
+   */
+  Command: async (interaction, participants) => {
+    //TODO: Implement this function @jhat0353
+  },
+  /**
+   * 
+   * @param {string} participants 
+   * @param {ParticipantManager} participants 
+   */
+  canSendGift: async (SenderID, participants) => {
+    //TODO: Implement this function @jhat0353
+  }
+}
+module.exports = gift;
