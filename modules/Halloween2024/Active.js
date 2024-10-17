a9f4ac7826ad96d031bd86c594dd70211d8be78e
SapphireSapphic <sophia.Victoroff@gmail.com>
2024-10-16 23:19:48 -0400

    Pull

---
+++ modules/Halloween2024/Active.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+//active should be set based on a file in the same directory as pristine waters called active.json. if it doesn't exist, it should be created with the value of false
+//if active.json exists
+let active;
+if (!fs.existsSync('./data/holiday/active.json')) {
+  fs.writeFileSync('./data/holiday/active.json', JSON.stringify({ active: false }));
+  active = false;
+} else {
+  active = require('../../data/holiday/active.json').active;
+}
+//if active.json is true, set active to true
+function setActive(bool) {
+  active = bool;
+  fs.writeFileSync('./data/holiday/active.json', JSON.stringify({ active: bool }));
+}
+
+module.exports = {
+  getActive: active,
+  setActive: setActive
+}
---
+++ modules/Halloween2024/Spam.js
@@ -0,0 +1,13 @@
+const { Message } = require("discord.js");
+
+module.exports = {
+  /**
+   * 
+   * @param {Message} msg 
+   * @param {ParticipantManager} participants 
+   * @returns {boolean} returns true if the message is spam
+   */
+  isSpam: async (msg, participants) => {
+    //TODO: Implement this function @jhat0353
+  }
+}
