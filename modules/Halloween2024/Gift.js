58cb415240c962a1cb334cba5146ed79230b2ad7
Jordan Hatcher <jhat0353@gmail.com>
2024-10-17 18:41:27 -0700

    Signed-off-by: Jordan Hatcher <jhat0353@gmail.com>

--- modules/Halloween2024/Gift.js
+++ modules/Halloween2024/Gift.js
@@ -4,7 +4,7 @@
  * Date: 2024
  */
 
-const ParticipantManager = require('participant.js');
+const ParticipantManager = require('./Participant.js');
 const Interaction = require('discord.js')
 
 function sendGift(interaction, participants) {
