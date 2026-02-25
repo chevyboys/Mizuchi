e26f05f0794a318cc88ba39a3a83b811043ca062
chevyboys <volcanicchevy@gmail.com>
2026-02-24 22:26:27 -0700

    fix: update user ID reference in processPurchaseCallback for Onyx role

--- shop/onyx_aura.js
+++ shop/onyx_aura.js
@@ -10,7 +10,7 @@ module.exports = new ShopItem(
   // YOU MUST MAKE SURE TO REPLY TO THE INTERACTION IN THE processPurchaseCallback, otherwise the user will see "This interaction failed" message after purchasing the item, which is not a good user experience. If the item doesn't actually do anything and you just want to confirm the purchase, you can reply with a simple message like "You have purchased [item name]!" or something like that.
   // YOU MUST also make sure to return a promise that resolves when the purchase has been processed, which in this case is just after we reply to the interaction. If you have any asynchronous code in the processPurchaseCallback, you should make sure to await it before returning, to ensure that the purchase is fully processed before the promise resolves.
   async (interaction) => {
-    await RoleInventory.addRoleToInventory(interaction.user.id, "902768080323772468"); // Onyx Role ID
+    await RoleInventory.addRoleToInventory(interaction.member, "902768080323772468"); // Onyx Role ID
     await interaction.reply({ content: "Use /inventory to equip the Onyx role", ephemeral: true });
     return Promise.resolve({ success: true });
   }
