d852cb6099a6dba5a1cc961d8fa8c541f70275d5
chevyboys <volcanicchevy@gmail.com>
2026-05-03 12:44:30 -0600

    Update nothing_plus_plus.js

--- shop/nothing_plus_plus.js
+++ shop/nothing_plus_plus.js
@@ -2,9 +2,7 @@ const ShopItem = require("../utils/Class.ShopItem");
 
 module.exports = new ShopItem(
   "Literally Nothing++",
-  "From the designer who brought you the original nothing that critics are raving about comes nothing++, all the same things you love about the original nothing, but now colored blue! 
-
-Warranty void if assumed to be something.",
+  "From the designer who brought you the original nothing that critics are raving about comes nothing++, all the same things you love about the original nothing, but now colored blue! \n\nWarranty void if assumed to be something.",
   20000, // the cost
   1, // Assuming 1 is the ID for the main currency used in the shop, and 2 is the id for test currency we are using for this item, which doesn't actually do anything yet
   //NOTE: The processPurchaseCallback is meant to handle giving the purchased item to the user, and any other side effects of purchasing the item. In this case, since Kudos doesn't actually do anything yet, we will just reply to the interaction to confirm the purchase.
