4fc3b874142c4d632898449422095c0a2de5914a
chevyboys <volcanicchevy@gmail.com>
2026-05-17 19:04:02 -0600

    More economy fixes

--- utils/Class.ShopItem.js
+++ utils/Class.ShopItem.js
@@ -86,7 +86,7 @@ class Item {
         const currencyEmoji = currency ? currency.emoji : "";
 
         //send a message in bot-logs channel about the purchase
-        let logChannel = await interaction.client.channels.fetch(interaction.guild.config.snowflakes.channels.botSpam);
+        let logChannel = await interaction.client.channels.fetch(interaction.client.config.snowflakes.channels.botSpam);
         let embed = u.embed().setTitle(`${interaction.user.tag} purchased ${this.name}`)
           .setDescription(`**Item:** ${this.name}\n**Price:** ${this.price} ${currencyEmoji} ${currencyName}\n**User:** <@${interaction.user.id}>\n They have ${userBalance.total - this.price} ${currencyEmoji} ${currencyName} remaining after this purchase.`)
           .setTimestamp();
