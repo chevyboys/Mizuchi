9132a992b3699e016eae40c14311f019c9d6a431
chevyboys <volcanicchevy@gmail.com>
2026-05-28 00:03:15 -0600

    Thank log updates

--- modules_common/Module.CommandInteraction.Thank.js
+++ modules_common/Module.CommandInteraction.Thank.js
@@ -31,7 +31,7 @@ let thankProcess = async (interaction) => {
   const numberOfPointsToGrant = 3;
 
   const currencies = await UtilsDatabase.Economy.getValidCurrencies(interaction.guild.id);
-  console.log(currencies);
+  console.log(JSON.stringify(currencies, null, 2));
   const TournamentPointsId = currencies.find(c => c.is_primary).id;
 
   await UtilsDatabase.Economy.newTransaction(member.id, TournamentPointsId, numberOfPointsToGrant, interaction.member.id, 'Thank');
