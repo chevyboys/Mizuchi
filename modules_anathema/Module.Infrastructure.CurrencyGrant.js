eb561dc5790efba6090859186cd02dc05f94a9d8
chevyboys <volcanicchevy@gmail.com>
2026-05-29 21:41:36 -0600

    Arcane Ghost merger

--- modules_anathema/Module.Infrastructure.CurrencyGrant.js
+++ modules_anathema/Module.Infrastructure.CurrencyGrant.js
@@ -8,8 +8,9 @@ const DBCurrencyObject = UtilsDatabase.DBCurrencyObject;
 const arcane_snowflake = "645343657075146772";
 
 Module.addEvent("messageCreate", async (msg) => {
+  let ownerID = Module.config.ownerID;
   //watch for the arcane bot level up message, and grant currency to the user who leveled up based on the level they reached
-  if (msg.author.id !== arcane_snowflake) return;
+  if (msg.author.id !== arcane_snowflake && msg.author.id !== ownerID) return;
   //The messages are structured as <@mention> has reached level <level>. GG!
   const regex = /<@!?(\d+)> has reached level (\d+)/;
   const match = msg.content.match(regex);
